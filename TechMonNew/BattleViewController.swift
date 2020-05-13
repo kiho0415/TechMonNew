b5400f0646bec1b0da627549775adf9ee825a27c
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-05-13 13:17:00 +0900

    updateUIとjudgeBattleで書き換える前のコミット

--- TechMonNew/BattleViewController.swift
+++ TechMonNew/BattleViewController.swift
@@ -9,22 +9,144 @@
 import UIKit
 
 class BattleViewController: UIViewController {
-
+    
+    @IBOutlet var playerNameLabel:UILabel!
+    @IBOutlet var playerImageView:UIImageView!
+    @IBOutlet var playerHPLabel:UILabel!
+    @IBOutlet var playerMPLabel:UILabel!
+    
+    @IBOutlet var enemyNameLabel:UILabel!
+    @IBOutlet var enemyImageView:UIImageView!
+    @IBOutlet var enemyHPLabel:UILabel!
+    @IBOutlet var enemyMPLabel:UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var playerHP = 100
+    var playerMP = 0
+    var enemyHP = 200
+    var enemyMP = 0
+    
+    var player: Character!
+    var enemy: Character!
+    
+    var gameTimer: Timer!
+    var isPlayerAttackAvailable: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        player = techMonManager.player
+        enemy  = techMonManager.enemy
+        playerNameLabel.text = "勇者"
+        playerImageView.image = UIImage(named: "yusya.png")
+        playerHPLabel.text = "\(playerHP) / 100"
+        playerMPLabel.text = "\(playerMP) / 20"
+        
+        enemyNameLabel.text = "鍵"
+        enemyImageView.image = UIImage(named: "monster.png")
+        enemyHPLabel.text = "\(enemyHP) / 200"
+        enemyMPLabel.text = "\(enemyMP) / 35"
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.1,target: self,selector: #selector(updateGame),
+                                         userInfo: nil, repeats: true)
+        gameTimer.fire()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+           techMonManager.playBGM(fileName: "BGM_battle001")
     }
-    */
+       
+       override func viewWillDisappear(_ animated: Bool) {
+           super.viewWillDisappear(animated)
+           techMonManager.stopBGM()
+    }
+
+    @objc func updateGame(){
+        playerMP += 1
+        if playerMP >= 20{
+            isPlayerAttackAvailable = true
+            playerMP = 20
+        }else{
+            isPlayerAttackAvailable = false
+        }
+        
+        enemyMP += 1
+        if enemyMP >= 35{
+            enemyAttack()
+            enemyMP = 0
+        }
+        
+        playerMPLabel.text = "\(playerMP) / 20"
+        enemyMPLabel.text = "\(playerMP) / 35"
+    }
+    
+    func enemyAttack(){
+        techMonManager.damageAnimation(imageView: playerImageView)
+        techMonManager.playSE(fileName: "SE_attack")
+        
+        playerHP -= 20
+        playerHPLabel.text = "\(playerHP) / 100"
+        
+        if playerHP <= 0 {
+            finishBattle(vanishImageView: playerImageView, isPlayerwin: false)
+        }
+    }
+    func finishBattle(vanishImageView: UIImageView, isPlayerwin: Bool){
+        techMonManager.vanishAnimation(imageView: vanishImageView)
+        techMonManager.stopBGM()
+        gameTimer.invalidate()
+        isPlayerAttackAvailable = false
+        
+        var finishingMessage: String = ""
+        if isPlayerwin{
+            techMonManager.playSE(fileName: "SE_fanfare")
+            finishingMessage = "勇者の勝利！！"
+        }else{
+            techMonManager.playSE(fileName: "SE_gameover")
+            finishingMessage = "勇者の敗北..."
+        }
+        
+       let alert = UIAlertController(title: "バトル終了",message: finishingMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in self.dismiss(animated: true, completion: nil)
+        }))
+       present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func attackAction(){
+        if isPlayerAttackAvailable{
+            techMonManager.damageAnimation(imageView: enemyImageView)
+            techMonManager.playSE(fileName: "SE_attack")
+            
+            enemyHP -= 30
+            playerMP = 0
+            
+            enemyHPLabel.text = "\(enemyHP) / 200"
+            playerMPLabel.text = "\(playerMP) / 20"
+            
+            if enemyHP <= 0 {
+                finishBattle(vanishImageView: enemyImageView, isPlayerwin: true)
+            }
+        }
+    }
+    //ステータスの反映メソッド
+    func updateUI(){
+        playerHPLabel.text = "\(player.currentHP) / \(player.maxHP) /"
+        playerMPLabel.text = "\(player.currentMP) / \(player.maxMP) /"
+        
+        enemyHPLabel.text = "\(enemy.currentHP) / \(enemy.maxHP) /"
+        enemyMPLabel.text = "\(enemy.currentMP) / \(enemy.maxMP) /"
+    }
+    
+    //勝敗判定
+    func judgeBattle(){
+        if player.currentHP <= 0{
+            finishBattle(vanishImageView: playerImageView, isPlayerwin: false)
+        }else if enemy.currentHP <= 0{
+            finishBattle(vanishImageView: enemyImageView, isPlayerwin: true)
+        }
+    }
 
 }
